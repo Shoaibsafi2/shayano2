c10ed67ed63a9ef32e0722231fb9bc2a651c8ecf
Shoaibsafi2 <97215018+Shoaibsafi2@users.noreply.github.com>
2022-05-30 23:20:44 +0500

    Add files via upload

---
+++ SHAYANO.py
@@ -0,0 +1,26 @@
+# Dump
+Dump Multi Free ❤
+
+𝙴𝙽𝙹𝙾𝚈 𝙵𝚁𝙴𝙴 ❤🥀
+
+pkg update
+
+pkg upgrade
+
+pkg install git
+
+pkg install python - python2
+
+pip install requests
+
+pip install mechanize
+
+pip install bs4
+
+rm -rf shayano
+
+git clone https://github.com/shayano/shayano
+
+cd shayano
+
+python shayano.py
